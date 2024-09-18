71fc8e4cc77f3b4128f54de33a824f6326f5bd63
Vikash2281 <vikash.singh@appventurez.com>
2024-09-18 17:58:04 +0530

    protect get email route

--- src/routes/gmailRouts.js
+++ src/routes/gmailRouts.js
@@ -4,11 +4,12 @@ const {
   oauth2callback,
   emailList,
 } = require("../controllers/gmailController");
+const { verifyAuthToken } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/auth-gmail", authGmail);
 router.get("/oauth2callback", oauth2callback);
-router.get("/emails", emailList);
+router.get("/emails", verifyAuthToken, emailList);
 
 module.exports = router;
